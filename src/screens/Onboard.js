17f5382e8f9ba96ba9b3c80245b4dd502bf0a302
piyush-gupta123 <95485177+piyush-gupta123@users.noreply.github.com>
2023-08-22 23:04:17 +0530

    Image Slider is complete

--- src/screens/Onboard.js
+++ src/screens/Onboard.js
@@ -1,47 +1,71 @@
 import {
   View,
   Text,
-  Image,
   StyleSheet,
   SafeAreaView,
   TouchableOpacity,
 } from "react-native";
 import React from "react";
 import COLORS from "../consts/colors";
-import startingImage from "../../assets/startingImage.png";
+import { SliderBox } from "react-native-image-slider-box";
+
+const images = [
+  require("../../assets/Boarding_Images/startingImage.png"),
+  require("../../assets/Boarding_Images/image2.jpg"),
+  require("../../assets/Boarding_Images/image3.png"),
+  require("../../assets/Boarding_Images/image4.png"),
+  require("../../assets/Boarding_Images/image5.jpg"),
+];
 
 export default function Onboard({ navigation }) {
   return (
     <SafeAreaView style={style.container}>
-      <Image source={startingImage} style={{ width: 300, height: 300 }} />
-      <TouchableOpacity
-        style={{ marginTop: 20, width: "80%" }}
-        onPress={() => navigation.navigate("HomeScreen")}
-      >
-        <View style={style.button}>
+      <View style={{height: "80%", justifyContent: "center", alignItems: "center"}}>
+        <SliderBox
+          images={images}
+          sliderBoxHeight={50}
+          dotColor={COLORS.primary}
+          inactiveDotColor="grey"
+          autoplay
+          circleLoop
+          paginationBoxVerticalPadding={30}
+          ImageComponentStyle={{
+            height: 400,
+            width: 400,
+            borderRadius: 50,
+            marginTop: 60,
+          }}
+        />
+        <TouchableOpacity
+          style={{ marginTop: 20, width: "100%" }}
+          onPress={() => navigation.navigate("HomeScreen")}
+        >
+          <View style={style.button}>
             <Text
               style={{ color: COLORS.white, fontSize: 20, fontWeight: 700 }}
             >
               Get Started
             </Text>
-        </View>
-      </TouchableOpacity>
+          </View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
 
 const style = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "column",
   },
   button: {
-    backgroundColor: "#007FFF",
-    padding: 15,
+    // backgroundColor: "#007FFF",
+    backgroundColor: COLORS.primary,
+    padding: 20,
     borderRadius: 20,
     justifyContent: "center",
     alignItems: "center",
+    width: "100%",
   },
 });
